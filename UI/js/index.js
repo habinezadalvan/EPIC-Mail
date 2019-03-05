17c805e1dfde2057c5e669de643d4465e0a10deb
habineza leon christian <habinezadalvan@gmail.com>
2019-03-05 12:46:37 +0200

    this commit disactivates javascript functionality to login button

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,15 +1,22 @@
 
-const loginInput = document.getElementById('loginPassword');
-const submitButton = document.getElementById('submit');
+// const loginInput = document.getElementById('loginPassword');
+// const submitButton = document.getElementById('submit');
 
-loginInput.addEventListener('input', ($event)=>{
-    if ($event.target.value.length >= 6 && $event.target.value.length <= 12){
-        submitButton.removeAttribute('disabled');
-        submitButton.innerHTML= '<a href="./HTML/home.html"></a></a>'
-    }else{
-        submitButton.setAttribute('disabled', 'true');
-    }
-})
+
+
+// loginInput.addEventListener('input', ($event)=>{
+//     $event.preventDefault();
+//     if ($event.target.value.length >= 6 && $event.target.value.length <= 12){
+//         submitButton.removeAttribute('disabled') 
+
+//     }else{
+//         submitButton.setAttribute('disabled','true');
+//     }
+// })
+
+// submitButton.addEventListener('click', ()=>{
+//     submitButton.innerHTML ='<a href="./HTML/home.html"></a>';
+// })
 
 
 
