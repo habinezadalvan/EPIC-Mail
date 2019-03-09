51aff9b6040ce10668227b8134d103b81aa5129f
habineza leon christian <habinezadalvan@gmail.com>
2019-03-09 20:52:46 +0200

    [feature #164515465] this commit creates login validation that enables the user to enter validated input

--- server/controllers/account.js
+++ server/controllers/account.js
@@ -1,6 +1,8 @@
 import signup from '../models/signup';
 import login from '../models/login';
 
+import loginValidation from '../helpers/loginValidation';
+
 const account = {
 
   userSignup(req, res, next) {
@@ -21,6 +23,12 @@ const account = {
     next();
   },
   userLogin(req, res, next) {
+    const { error } = loginValidation.validateLogin(req.body);
+    if (error) {
+      res.status(400).send(error.details[0].message);
+      return;
+    }
+
     const newId = Number(login.length + 35);
     const newpwd = toString(req.body.password);
     const loginAccount = {
