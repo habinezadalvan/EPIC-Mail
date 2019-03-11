ec5d9c1d79bc5428b9ccf1310d9af1d60e8e9282
habineza leon christian <habinezadalvan@gmail.com>
2019-03-11 16:52:57 +0200

    [feature #164208202] this commit validates the user input before logging in

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,22 +1,16 @@
 
-// const loginInput = document.getElementById('loginPassword');
-// const submitButton = document.getElementById('submit');
+const loginInput = document.getElementById('loginPassword');
+const submitButton = document.getElementById('submit');
 
+loginInput.addEventListener('input', ($event) => {
+  $event.preventDefault();
+  if ($event.target.value.length >= 6 && $event.target.value.length <= 12) {
+    submitButton.removeAttribute('disabled');
+  } else {
+    submitButton.setAttribute('disabled', 'true');
+  }
+});
 
-
-// loginInput.addEventListener('input', ($event)=>{
-//     $event.preventDefault();
-//     if ($event.target.value.length >= 6 && $event.target.value.length <= 12){
-//         submitButton.removeAttribute('disabled') 
-
-//     }else{
-//         submitButton.setAttribute('disabled','true');
-//     }
-// })
-
-// submitButton.addEventListener('click', ()=>{
-//     submitButton.innerHTML ='<a href="./HTML/home.html"></a>';
-// })
-
-
-
+submitButton.addEventListener('click', () => {
+  submitButton.innerHTML = '<a href="./HTML/home.html"></a>';
+});
