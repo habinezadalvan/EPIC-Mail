3d3e230857df301e53735d4cddf971f6a8bfd5ca
habineza leon christian <habinezadalvan@gmail.com>
2019-03-13 22:48:53 +0200

    [bug #164609196] this commit renames some variables and modifies comments in code and set .env file

--- server/models/login.js
+++ server/models/login.js
@@ -1,4 +1,4 @@
-// this line creates an empty array that will contain all sent input from the user for login.
+// an empty array that will contain all sent input from the user for login.
 const login = [];
 
 export default login;
